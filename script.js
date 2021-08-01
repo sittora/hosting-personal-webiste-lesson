7aa29aa61d9dec92115c6a9e8bdb5858bb33eb9f
sittora <sittora@gmail.com>
2021-08-01 18:49:56 -0400

    updated html css event listenerl

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript"; 
-document.querySelector('body').appendChild(h2); 
+let image = document.querySelector('img');
+
+
+function flipImage() {
+    image.style.transform = "scaleX(-1)";
+  }
+  
+  image.addEventListener('mouseenter', flipImage); 
+
+  function removeReverse() {
+    image.style.removeProperty('transform');
+  }
+  
+  image.addEventListener('mouseleave', removeReverse); 
+
